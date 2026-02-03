fd3d6b18ba01afcf51258246443138bdb997733e
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-02-03 07:59:17 -0800

    finished /api/sign_up ✅

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config({ path: "../.env" });
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
 
 const db_URL = process.env.MONGO_URL;
 
@@ -11,30 +15,37 @@ mongoose
   .catch((err) => console.error("database failed to connect", err));
 
 const userSchema = new mongoose.Schema({
-  user_id: { type: Number, unique:true, required: true, min: 1 },
+  user_id: { type: Number, unique: true, required: true, min: 1 },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   role: { type: String, default: "user", required: true },
   trusted_ips: { type: [String], default: [] },
   created_at: { type: Date, default: Date.now },
 });
+const User = mongoose.model("user", userSchema);
 
-async function createUser() {
-  const User = mongoose.model("User", userSchema);
-  const user = new User({
-    user_id: 2,
-    email: "test2@gmail.com",
-    password: "test2123",
-    trusted_ips: "127:0:0:12",
-  });
-
-  const result = await user.save();
-  console.log(result);
-}
-createUser();
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
+
+app.post("/api/sign_up", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const user = new User({
+      user_id: req.body._id,
+      email: req.body.email,
+      password: hashedPassword,
+      trusted_ips: req.ip,
+    });
+    const savedUser = await user.save();
+    res.status(201).send(savedUser);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
